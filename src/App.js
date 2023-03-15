73aeb9e247963524a1688de8c98fa1bc2ed30aa4
Nikolaj Hoggins <mollevip@gmail.com>
2023-03-15 13:18:58 +0100

    Done ish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/Card";
+
+const data = [
+  {
+    name: "Nikolaj Hoggins",
+    position: "Lead Developer",
+    age: 21,
+    picture:
+      "https://media.licdn.com/dms/image/C5603AQFwIDp_8ndonA/profile-displayphoto-shrink_200_200/0/1565083485236?e=1684368000&v=beta&t=3R_ZptNUJajS1raUu43eg5pPEMOa1KuqtcDUwo7UNSY",
+    email: { home: "nikolaj@home.com", work: "nikolaj@work.com" },
+  },
+  {
+    name: "Mike Møller Madsen",
+    position: "CEO",
+    age: "Gammel Mand",
+    picture:
+      "https://media.licdn.com/dms/image/C4E03AQENwN-KTOLohQ/profile-displayphoto-shrink_800_800/0/1619703800663?e=1684368000&v=beta&t=A7XXxnl1ipDi9uoaGb_NnIxYRSrztiUlZ6IfhjV0clI",
+    email: { home: "mike@home.com", work: "mike@work.com" },
+  },
+  {
+    name: "Sebastian Bach Anders",
+    position: "CEO",
+    age: "22",
+    picture:
+      "https://media.licdn.com/dms/image/C4D03AQFmMx3ktWYhIQ/profile-displayphoto-shrink_800_800/0/1601901074260?e=1684368000&v=beta&t=P-BoFNC41IcqpfERNt-fXCoI6m3QsjFM4waIkl7SrH0",
+    email: { home: "sebastian@home.com", work: "sebastian@work.com" },
+  },
+  {
+    name: "Rasmus Rosengreen",
+    position: "CEO",
+    age: "22",
+    picture:
+      "https://media.licdn.com/dms/image/C4D03AQFmMx3ktWYhIQ/profile-displayphoto-shrink_800_800/0/1601901074260?e=1684368000&v=beta&t=P-BoFNC41IcqpfERNt-fXCoI6m3QsjFM4waIkl7SrH0",
+    email: { home: "rasmus@home.com", work: "rasmus@work.com" },
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen flex justify-center items-center text-gray-800">
+      <div className="flex flex-col items-center">
+        <p className=" text-2xl font-bold mb-14">Welcome to Bricksite!</p>
+        <div className="flex flex-col gap-4">
+          {data.map((c) => (
+            <Card card={c} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,35 @@
+const Card = ({ card }) => {
+  const sendEmail = (emails) => {
+    const workEmail = emails.mail.work;
+    alert("email sent to " + workEmail);
+  };
+
+  return (
+    <div className="flex flex-col text-gray-800 border p-3 shadow-md rounded-md items-end hover:-translate-y-1 transition-all">
+      <div className="flex items-center w-full">
+        <div>
+          <img
+            src={card.picture}
+            alt="employee"
+            className="h-16 w-16 object-cover rounded-full"
+          />
+        </div>
+        <div className="flex flex-col ml-8">
+          <p className=" text-xl">{card.name}</p>
+          <p>{card.position}</p>
+          <p>
+            Age: {card.age} years <span className=" text-gray-600">/</span>{" "}
+            about {card.age * 12} months
+          </p>
+        </div>
+      </div>
+      <button
+        className="border p-2 rounded-sm mt-2"
+        onClick={() => sendEmail(card.email)}
+      >
+        Send email to work
+      </button>
+    </div>
+  );
+};
+export default Card;
